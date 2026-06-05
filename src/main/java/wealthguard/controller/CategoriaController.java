1835f94666991ebabfdf5dd17b888fe70a582a0c
AntonioPando <antoniogpando@gmail.com>
2026-06-05 14:15:07 +0200

    feat(categoria): add CrossOrigin annotation to CategoriaController

--- src/main/java/wealthguard/controller/CategoriaController.java
+++ src/main/java/wealthguard/controller/CategoriaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,7 @@ import wealthguard.dto.CategoriaResponseDTO;
 import wealthguard.service.ICategoriaService;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS}, allowedHeaders = "*")
 @RequestMapping("/categorias")
 public class CategoriaController {
 
@@ -52,4 +55,3 @@ public class CategoriaController {
     }
 
 }
-
