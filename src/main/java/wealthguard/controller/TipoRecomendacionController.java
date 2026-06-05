55a01ec409a7e3b189d010e4d303a805d7cef96f
carlosalbertoparejo <parejoc@ymail.com>
2026-06-05 14:46:35 +0200

    tipo recomendación controlador

--- src/main/java/wealthguard/controller/TipoRecomendacionController.java
+++ src/main/java/wealthguard/controller/TipoRecomendacionController.java
@@ -1,6 +1,39 @@
 package wealthguard.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import wealthguard.dto.TipoRecomendacionRequestDTO;
+import wealthguard.dto.TipoRecomendacionResponseDTO;
+import wealthguard.service.ITipoRecomendacionService;
 public class TipoRecomendacionController {
+    @RestController
+    @RequestMapping("/tipo-recomendacion")
 
+    public class TipoRecomendacionController {
+    
+        @Autowired
+        private ITipoRecomendacionService service;
+    
+        @PostMapping
+        public TipoRecomendacionResponseDTO crear(@RequestBody TipoRecomendacionRequestDTO dto){
+            return service.crearTipoRecomendacion(dto); 
+        }
+
+        @GetMapping({"/{id}"})
+        public TipoRecomendacionResponseDTO obtenerTipoRecomendacionPorId(@PathVariable Integer id) {
+            return service.obtenerTipoRecomendacionPorId(id);
+        }
+
+        @PutMapping("/{id}")
+        public TipoRecomendacionResponseDTO actualizarTipoRecomendacion
+            (@PathVariable Integer id, 
+            @RequestBody TipoRecomendacionRequestDTO dto) {
+                return service.actualizarTipoRecomendacion(id, dto);
+        }
+
+        @DeleteMapping("/{id}")
+        public void eliminarTipoRecomendacion(@PathVariable Integer id) {
+            service.eliminarTipoRecomendacion(id);
+        }
 }
 
