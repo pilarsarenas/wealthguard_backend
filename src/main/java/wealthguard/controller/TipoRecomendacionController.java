df3759d38be919f277040671f30ac6cff3e1d5c1
carlosalbertoparejo <parejoc@ymail.com>
2026-06-06 14:22:28 +0200

    tiporecomendacion service e impl arreglados

--- src/main/java/wealthguard/controller/TipoRecomendacionController.java
+++ src/main/java/wealthguard/controller/TipoRecomendacionController.java
@@ -1,39 +1,45 @@
 package wealthguard.controller;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import wealthguard.dto.TipoRecomendacionRequestDTO;
 import wealthguard.dto.TipoRecomendacionResponseDTO;
 import wealthguard.service.ITipoRecomendacionService;
+
+@RestController
+@RequestMapping("/tipo-recomendacion")
 public class TipoRecomendacionController {
-    @RestController
-    @RequestMapping("/tipo-recomendacion")
 
-    public class TipoRecomendacionController {
-    
-        @Autowired
-        private ITipoRecomendacionService service;
-    
-        @PostMapping
-        public TipoRecomendacionResponseDTO crear(@RequestBody TipoRecomendacionRequestDTO dto){
-            return service.crearTipoRecomendacion(dto); 
-        }
+    @Autowired
+    private ITipoRecomendacionService service;
 
-        @GetMapping({"/{id}"})
-        public TipoRecomendacionResponseDTO obtenerTipoRecomendacionPorId(@PathVariable Integer id) {
-            return service.obtenerTipoRecomendacionPorId(id);
-        }
+    @PostMapping
+    public TipoRecomendacionResponseDTO crear(@RequestBody TipoRecomendacionRequestDTO dto){
+        return service.crearTipoRecomendacion(dto); 
+    }
 
-        @PutMapping("/{id}")
-        public TipoRecomendacionResponseDTO actualizarTipoRecomendacion
-            (@PathVariable Integer id, 
-            @RequestBody TipoRecomendacionRequestDTO dto) {
-                return service.actualizarTipoRecomendacion(id, dto);
-        }
+    @GetMapping
+    public List<TipoRecomendacionResponseDTO> listarTodos() {
+        return service.listarTodos();
+    }
 
-        @DeleteMapping("/{id}")
-        public void eliminarTipoRecomendacion(@PathVariable Integer id) {
-            service.eliminarTipoRecomendacion(id);
-        }
+    @GetMapping("/{id}")
+    public TipoRecomendacionResponseDTO obtenerTipoRecomendacionPorId(@PathVariable Integer id) {
+        return service.obtenerTipoRecomendacionPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public TipoRecomendacionResponseDTO actualizarTipoRecomendacion(
+        @PathVariable Integer id, 
+        @RequestBody TipoRecomendacionRequestDTO dto) {
+        return service.actualizarTipoRecomendacion(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarTipoRecomendacion(@PathVariable Integer id) {
+        service.eliminarTipoRecomendacion(id);
+    }
 }
-
